e2fac08f6036a0cae034879d8faf870345472afd
div-3 <div-3@mail.ru>
2023-06-29 23:42:53 +0800

    Task 2. Add season calculation to Season.

--- src/main/java/Task2/Season.java
+++ src/main/java/Task2/Season.java
@@ -7,16 +7,18 @@ import java.util.Random;
 public class Season {
     private LocalDate currentDate;
 
-    public Seasons getSeason(){
-        switch (Integer.parseInt ((currentDate = LocalDate.now()).getMonth().toString())) {
-            case 1, 2, 12:
-                return Seasons.WINTER;
-            case 3, 4, 5:
-                return Seasons.SPRING;
-            case 6, 7, 8:
-                return Seasons.SUMMER;
-            default:
-                return Seasons.AUTUMN;
-        }
+    public Seasons getCurrentSeason(){
+        currentDate = LocalDate.now().minusMonths(new Random().nextInt(0, 12));
+        System.out.println(currentDate.getMonth());
+        return getSeasonByDate(currentDate);
+    }
+
+    public Seasons getSeasonByDate(LocalDate localDate){
+        return switch (localDate.getMonthValue()) {
+            case 1, 2, 12 -> Seasons.WINTER;
+            case 3, 4, 5 -> Seasons.SPRING;
+            case 6, 7, 8 -> Seasons.SUMMER;
+            default -> Seasons.AUTUMN;
+        };
     }
 }
